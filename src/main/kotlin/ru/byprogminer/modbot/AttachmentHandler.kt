e3ae1b98c377bcb57d9d9d2584ead8546632098e
Eridan Domoratskiy <eridan200@mail.ru>
2019-08-26 14:48:13 +0300

    Fix attachment handler

--- src/main/kotlin/ru/byprogminer/modbot/AttachmentHandler.kt
+++ src/main/kotlin/ru/byprogminer/modbot/AttachmentHandler.kt
@@ -10,5 +10,5 @@ interface AttachmentHandler {
     fun video(uri: URI): Attachment
     fun audio(uri: URI): Attachment
     fun document(uri: URI): Attachment
-    fun sticker(uri: URI): Attachment
+    fun sticker(id: Long): Attachment
 }
